33e2926ca5b836af334dfed025cf9e7fc5fa643c
Paul Vorbach <paul@vorba.ch>
2014-12-08 01:58:59 +0100

    Correctly handle comment counts

--- src/main/scala/de/vorb/tala/actors/APIHandler.scala
+++ src/main/scala/de/vorb/tala/actors/APIHandler.scala
@@ -1,9 +1,11 @@
 package de.vorb.tala.actors
 
 import akka.actor.Actor
-
 import de.vorb.tala.actors.Messages.GetCommentCount
 import de.vorb.tala.actors.Messages.GetComments
+import org.mashupbots.socko.events.HttpResponseStatus
+import de.vorb.tala.db.DBPool
+import de.vorb.tala.cache.Caches
 
 class APIHandler extends Actor {
     def receive = {
@@ -15,8 +17,17 @@ class APIHandler extends Actor {
             resp.write(s"$quantity newest comments.")
             context.stop(self)
 
-        case GetCommentCount(resp, Some(document)) =>
-            resp.write(s"$document has x comments.")
+        case GetCommentCount(resp, Some(uri)) =>
+            Caches.commentCountsCache.get(uri) match {
+                case Some(count) =>
+                    resp.write(s"{count:$count}", "application/json")
+                case None =>
+                    resp.status = HttpResponseStatus.INTERNAL_SERVER_ERROR
+                    resp.write("{error:\"db problem\"}", "application/json")
+            }
+
+            println(Caches.commentCountsCache)
+
             context.stop(self)
 
         case GetCommentCount(resp, None) =>
