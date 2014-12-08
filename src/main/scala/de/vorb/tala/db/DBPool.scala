a54202c49f43f8b0b82b4d9ae2f4658783cd0f9d
Paul Vorbach <paul@vorba.ch>
2014-12-08 01:57:51 +0100

    A database pool

---
+++ src/main/scala/de/vorb/tala/db/DBPool.scala
@@ -0,0 +1,19 @@
+package de.vorb.tala.db
+
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import java.sql.Connection
+
+object DBPool {
+    private val pool = new ComboPooledDataSource()
+    pool.setDriverClass("org.sqlite.JDBC")
+    pool.setJdbcUrl("jdbc:sqlite:/Users/Paul/Desktop/comments.db")
+
+    pool.setMinPoolSize(2)
+    pool.setAcquireIncrement(2)
+    pool.setMaxPoolSize(10)
+    pool.setMaxIdleTime(60)
+
+    def getConnection: Connection = pool.getConnection
+
+    def close(): Unit = pool.close()
+}
