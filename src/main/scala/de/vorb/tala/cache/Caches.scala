e062163e31bcb7bcc631d683cadea7cb4a423a01
Paul Vorbach <paul@vorba.ch>
2014-12-09 01:41:52 +0100

    Improve comment count cache

--- src/main/scala/de/vorb/tala/cache/Caches.scala
+++ src/main/scala/de/vorb/tala/cache/Caches.scala
@@ -4,21 +4,23 @@ import de.vorb.util.Cache
 import de.vorb.tala.db.DBPool
 
 object Caches {
-    val commentCountsCache: Cache[String, Option[Long]] =
+    val commentCountsCache: Cache[String, Long] =
         Cache((uri: String) => {
             val conn = DBPool.getConnection
             val stmt = conn.prepareStatement("""|SELECT COUNT(*) as count
                                                 |FROM threads, comments
-                                                |WHERE threads.uri = ?
-                                                |  AND threads.id = comments.tid;
+                                                |WHERE
+                                                |  threads.uri = ? AND
+                                                |  threads.id = comments.tid;
                                                 |""".stripMargin)
             stmt.setQueryTimeout(30) // TODO configurable timeout?
             stmt.setString(1, uri)
             val results = stmt.executeQuery()
 
+            // get the count from the result
             val count =
-                if (results.next()) Some(results.getLong(1))
-                else None
+                if (results.next()) results.getLong(1)
+                else -1L // invalid value
 
             results.close()
 
