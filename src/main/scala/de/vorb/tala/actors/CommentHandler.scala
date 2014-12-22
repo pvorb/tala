78be098c7f81d2925a80986b4b46e287de064800
Paul Vorbach <paul@vorba.ch>
2014-12-22 11:23:55 +0100

    Fix CommentHandler

--- src/main/scala/de/vorb/tala/actors/CommentHandler.scala
+++ src/main/scala/de/vorb/tala/actors/CommentHandler.scala
@@ -6,28 +6,27 @@ import java.util.HashMap
 import java.util.List
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
-
 import scala.compat.Platform
-
 import akka.actor.Actor
-
 import org.json.simple.JSONObject
 import org.json.simple.JSONValue
 import org.json.simple.parser.JSONParser
 import org.mashupbots.socko.events.HttpRequestEvent
 import org.mashupbots.socko.events.HttpResponseMessage
 import org.mashupbots.socko.events.HttpResponseStatus
-
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
-
 import de.vorb.tala.Utils
 import de.vorb.tala.actors.Messages.GetComments
 import de.vorb.tala.actors.Messages.ListComments
 import de.vorb.tala.actors.Messages.PostComment
 import de.vorb.tala.db.DBPool
-import de.vorb.tala.model.Comment
+import de.vorb.tala.model.CommentResult
+import de.vorb.tala.Sanitizer
+import scala.util.Success
+import scala.util.Failure
+import de.vorb.tala.model.CommentRequest
 
 class CommentHandler extends Actor {
     def receive = {
@@ -55,62 +54,14 @@ class CommentHandler extends Actor {
             val jsonObj = new JSONParser().parse(jsonStr)
                 .asInstanceOf[JSONObject]
 
-            // add the corresponding thread
-            val conn = DBPool.getConnection
-            val createThread = conn.prepareStatement(
-                """|INSERT IGNORE INTO threads
-                   |  (uri, title)
-                   |VALUES
-                   |  (?, ?);""".stripMargin)
-            createThread.setString(1, uri)
-            createThread.setString(2,
-                jsonObj.get("threadTitle").asInstanceOf[String])
-            createThread.execute()
-
-            // get the id of the inserted thread
-            val tid = {
-                val keys = createThread.getGeneratedKeys
-                if (keys.next()) {
-                    // if a new entry was made, use the new id
-                    keys.getLong("id")
-                } else {
-                    // otherwise do a SELECT to get the id of the thread
-                    val getThreadId = conn.prepareStatement(
-                        "SELECT id FROM threads WHERE uri = ?;")
-                    getThreadId.setString(1, uri)
-                    val result = getThreadId.executeQuery()
-                    if (!result.next()) {
-                        throw new Exception("race condition")
-                    }
-                    result.getLong("id")
-                }
+            Sanitizer.sanitizeComment(jsonObj) match {
+                case Success(comment) =>
+                    postComment(uri, comment,
+                        jsonObj.get("threadTitle").asInstanceOf[String])
+                case Failure(t) =>
+                    Utils.writeThrowable(http.response, t)
             }
 
-            // create the new comment
-            val createComment = conn.prepareStatement(
-                """|INSERT INTO comments
-                   |  (tid, parent, created, modified, mode, remote_addr,
-                   |    text, author, email, website)
-                   |VALUES
-                   |  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""".stripMargin)
-
-            createComment.setLong(1, tid)
-            createComment.setLong(2, jsonObj.get("parent").asInstanceOf[Long])
-            createComment.setDouble(3, Utils.dateToFloat(Platform.currentTime))
-            createComment.setDouble(4, 0d)
-            createComment.setInt(5, 1)
-            createComment.setString(6,
-                jsonObj.get("remoteAddress").asInstanceOf[String])
-            createComment.setString(7,
-                jsonObj.get("text").asInstanceOf[String])
-            createComment.setString(8,
-                jsonObj.get("author").asInstanceOf[String])
-            createComment.setString(9,
-                jsonObj.get("email").asInstanceOf[String])
-            createComment.setString(10,
-                jsonObj.get("website").asInstanceOf[String])
-
-            createComment.execute()
         } catch {
             case _: Throwable =>
                 http.response.write(HttpResponseStatus.BAD_REQUEST)
@@ -119,13 +70,59 @@ class CommentHandler extends Actor {
         }
     }
 
-    def sanitizeComment(obj: JSONObject): Boolean = {
-        val text = obj.get("text").asInstanceOf[String]
+    def postComment(uri: String, comment: CommentRequest,
+                    threadTitle: String): Unit = {
 
-        val author = obj.get("author").asInstanceOf[String]
-        val email = obj.get("email").asInstanceOf[String]
+        // add the corresponding thread
+        val conn = DBPool.getConnection
+        val createThread = conn.prepareStatement(
+            """|INSERT IGNORE INTO threads
+               |  (uri, title)
+               |VALUES
+               |  (?, ?);""".stripMargin)
+        createThread.setString(1, uri)
+        createThread.setString(2, threadTitle)
+        createThread.execute()
 
-        true
+        // get the id of the inserted thread
+        val tid = {
+            val keys = createThread.getGeneratedKeys
+            if (keys.next()) {
+                // if a new entry was made, use the new id
+                keys.getLong("id")
+            } else {
+                // otherwise do a SELECT to get the id of the thread
+                val getThreadId = conn.prepareStatement(
+                    "SELECT id FROM threads WHERE uri = ?;")
+                getThreadId.setString(1, uri)
+                val result = getThreadId.executeQuery()
+                if (!result.next()) {
+                    throw new Exception("race condition")
+                }
+                result.getLong("id")
+            }
+        }
+
+        // create the new comment
+        val createComment = conn.prepareStatement(
+            """|INSERT INTO comments
+               |  (tid, parent, created, modified, mode, remote_addr,
+               |    text, author, email, website)
+               |VALUES
+               |  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""".stripMargin)
+
+        createComment.setLong(1, tid)
+        createComment.setLong(2, comment.parent)
+        createComment.setDouble(3, Utils.dateToFloat(Platform.currentTime))
+        createComment.setDouble(4, 0d)
+        createComment.setInt(5, 1)
+        createComment.setString(6, comment.remoteAddress)
+        createComment.setString(7, comment.text)
+        createComment.setString(8, comment.author)
+        createComment.setString(9, comment.email)
+        createComment.setString(10, comment.website)
+
+        createComment.execute()
     }
 }
 
@@ -167,7 +164,7 @@ object CommentHandler {
                 stmt.setQueryTimeout(30)
                 val results = stmt.executeQuery()
 
-                val comments = new ArrayList[Comment]()
+                val comments = new ArrayList[CommentResult]()
                 while (results.next()) {
                     val id = results.getLong("id")
                     val parent = results.getLong("parent")
@@ -179,7 +176,7 @@ object CommentHandler {
                     val author = results.getString("author")
                     val email = results.getString("email")
                     val website = results.getString("website")
-                    comments.add(Comment(Some(id), parent, created, modified,
+                    comments.add(CommentResult(id, parent, created, modified,
                         text, author, email, website))
                 }
 
@@ -189,7 +186,7 @@ object CommentHandler {
             }
         })
 
-    def wrapListInObject(name: String, list: List[Comment]): String = {
+    def wrapListInObject(name: String, list: List[CommentResult]): String = {
         val obj = new HashMap[String, AnyRef](1)
         obj.put(name, list)
         JSONValue.toJSONString(obj)
