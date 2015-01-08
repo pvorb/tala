d91224e197aab6b48c75ec75fcb64c841b507fa2
Paul Vorbach <paul@vorba.ch>
2015-01-09 00:17:40 +0100

    Replace email by emailHash

--- src/main/scala/de/vorb/tala/model/CommentRequest.scala
+++ src/main/scala/de/vorb/tala/model/CommentRequest.scala
@@ -3,4 +3,4 @@ package de.vorb.tala.model
 import java.util.Date
 
 case class CommentRequest(parent: Long, text: String, author: String,
-                          email: String, website: String, remoteAddress: String)
+                          emailHash: String, website: String, remoteAddress: String)
